2fcbe5714d2f6b582e2ff0c3751c48dbaee1cfcb
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-27 16:51:51 +0200

    Improved readt data script

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import glob
 import os
 
@@ -9,5 +10,6 @@ def read_all_data():
     data_list = []
     for file in filenames:
         df = pd.read_csv(file, index_col=None, header=0)
-        data_list.append(df)
+        train, valid, test = np.split(df, [round(len(df) * 2 / 3), round(len(df) * 5 / 6)])
+        data_list.append({'train': train, 'valid': valid, 'test': test})
     return data_list
