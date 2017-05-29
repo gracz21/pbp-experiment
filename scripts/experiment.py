29b5735cec8b8877cc2a0b5d4904432b502776c2
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-29 17:14:44 +0200

    Changed main experiment function

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -1,10 +1,12 @@
 from scripts.read_data import read_all_data
 from scripts.decision_tree import DecisionTree
+from scripts.random_forest import RandomForest
+from scripts.gradient_tree_boosting import GradientTreeBoosting
 
 
 def experiment():
     data_list = read_all_data()
-    classifiers_groups = [DecisionTree(data_list)]
+    classifiers_groups = [DecisionTree(data_list), RandomForest(data_list), GradientTreeBoosting(data_list)]
 
     results = []
     for classifier in classifiers_groups:
