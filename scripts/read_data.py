edea8405a3966ba19c2d89b4f6563e38d4b949ae
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-29 16:25:43 +0200

    Customized read data to read given datasets

--- scripts/read_data.py
+++ scripts/read_data.py
@@ -5,11 +5,11 @@ import os
 
 
 def read_all_data():
-    path = r'./data'
+    path = r'./datasets/data'
     filenames = glob.glob(os.path.join(path, "*.csv"))
     data_list = []
     for file in filenames:
-        df = pd.read_csv(file, index_col=None, header=0)
+        df = pd.read_csv(file, sep=';', header=None, na_values='?')
         train, valid, test = np.split(df, [round(len(df) * 2 / 3), round(len(df) * 5 / 6)])
         data_list.append({'train': train, 'valid': valid, 'test': test})
     return data_list
